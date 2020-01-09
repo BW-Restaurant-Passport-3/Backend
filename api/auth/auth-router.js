4f80d4fa7fc4a7b4fed674844af2b2154eeb32e8
alfonsoruiz <1800ruiz@gmail.com>
2020-01-09 09:54:20 -0800

    Setup up login route to return user on response upon login

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -36,7 +36,7 @@ router.post('/login', async (req, res) => {
     if (user && bcrypt.compareSync(password, user.password)) {
       const token = generateToken(user);
 
-      res.status(200).json({ message: 'Login successful', token });
+      res.status(200).json({ user, token });
     } else {
       res.status(401).json({ message: 'Invalid credentials' });
     }
